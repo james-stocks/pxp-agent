18b808975764161ad239c31386f44497e39b0e86
james-stocks <james.stocks@puppetlabs.com>
2016-02-05 09:34:49 +0000

    (PCP-230) Acceptance - longer message expiries for RPC commands
    Increase the message expiry for RPC command messages, default 5 seconds is too short

--- acceptance/lib/pxp-agent/test_helper.rb
+++ acceptance/lib/pxp-agent/test_helper.rb
@@ -264,7 +264,7 @@ def rpc_blocking_request(broker, targets,
   message = PCP::Message.new({
     :message_type => 'http://puppetlabs.com/rpc_blocking_request',
     :targets => targets
-  })
+  }).expires(60)
 
   message.data = {
     :transaction_id => SecureRandom.uuid,
@@ -385,7 +385,7 @@ def rpc_non_blocking_request(broker, targets,
   message = PCP::Message.new({
     :message_type => 'http://puppetlabs.com/rpc_non_blocking_request',
     :targets => targets
-  })
+  }).expires(60)
 
   message.data = {
     :transaction_id => SecureRandom.uuid,
