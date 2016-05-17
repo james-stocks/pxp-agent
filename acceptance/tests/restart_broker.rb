150308868e7a3d4253ab9d57bdde7cf136882773
james-stocks <james.stocks@puppetlabs.com>
2016-05-17 11:48:34 +0100

    (WIP) Simplify restart broker test

--- acceptance/tests/restart_broker.rb
+++ acceptance/tests/restart_broker.rb
@@ -8,11 +8,7 @@ step 'On master, stop then restart the broker' do
   run_pcp_broker(master)
 end
 
-step 'On each agent, test that a 2nd association has occurred' do
-  show_pcp_logs_on_failure do
-    agents.each_with_index do |agent|
-      assert(is_not_associated?(master, "pcp://#{agent}/agent"),
-             "Agent identity pcp://#{agent}/agent for agent host #{agent} does not appear in pcp-broker's client inventory")
-    end
-  end
+step 'Perform an inventory_request' do
+  assert(is_not_associated?(master, "pcp://example.com/agent"),
+        "Agent identity pcp://#{agent}/agent for agent host #{agent} does not appear in pcp-broker's client inventory")
 end
